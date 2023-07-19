81a96e139115a007a8c6791cbb00263aa8bf9f54
marcin <marcin220798@gmail.com>
2023-07-19 18:22:10 +0200

    Changed the name of the folder containing a project due to errors. Angular app set correctly

--- MechanicalWorkshop.UI/AngularUI/src/app/app.component.spec.ts
+++ MechanicalWorkshop.UI/AngularUI/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UserInterface'`, () => {
+  it(`should have as title 'AngularUI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UserInterface');
+    expect(app.title).toEqual('AngularUI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('UserInterface app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularUI app is running!');
   });
 });
