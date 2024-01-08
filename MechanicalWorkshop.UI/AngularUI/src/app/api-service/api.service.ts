fc6615e8f88349eea3737463285056cf28b31fc0
marcin <marcin220798@gmail.com>
2024-01-08 01:43:50 +0100

    [WENT TO PROD] Adjust angular project for production. Add Some generated backend dependencies and configs.

--- MechanicalWorkshop.UI/AngularUI/src/app/api-service/api.service.ts
+++ MechanicalWorkshop.UI/AngularUI/src/app/api-service/api.service.ts
@@ -1,21 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ICustomer } from 'src/app/interfaces/icustomer'
 import { IRepair } from '../interfaces/irepair';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
+  apiUrl: string = environment.apiUrl;
+  subscriptionKey: string = '424bbda15e0a46cb8903c435ac21cf6f';
+  headers = new HttpHeaders({ 'Ocp-Apim-Subscription-Key': this.subscriptionKey });
 
   constructor(private httpClient: HttpClient) { }
 
   getAllCustomers(): Observable<ICustomer[]> {
-    return this.httpClient.get<ICustomer[]>('https://localhost:7037/api/v1/Customer/customers');
+    return this.httpClient.get<ICustomer[]>(`${this.apiUrl}/v1/Customer/customers`, { headers: this.headers });
   }
 
   getAllRepairs(): Observable<IRepair[]> {
-    return this.httpClient.get<IRepair[]>('https://localhost:7037/api/v1/Repair/Repairs');
+    return this.httpClient.get<IRepair[]>(`${this.apiUrl}/v1/Repair/Repairs`, { headers: this.headers });
   }
 }
---
+++ MechanicalWorkshop.UI/AngularUI/src/app/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: "https://mechanicalworkshop2.azurewebsites.net/api"
+};
